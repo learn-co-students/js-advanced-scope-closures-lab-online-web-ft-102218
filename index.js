65c4f430dddc52ff294f89027a0750c107b4e8a0
kodyclemens <kody.clemens@gmail.com>
2019-02-08 19:05:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 function produceDrivingRange(range){
   return function(start, end) {
-    let x = start.split("th")[0];
-    let y = end.split("th")[0];
-    x = Number(x);
-    y = Number(y);
-    let distance = y - x;
-    
-    if (distance > range) {
-      return `${distance - range} blocks out of range`;
+    let x = parseInt(start);
+    let y = parseInt(end);
+    let difference = y - x;
+
+    if (difference > range) {
+      return `${difference - range} blocks out of range`;
     } else {
-      return `within range by ${range - distance}`;
+      return `within range by ${range - difference}`;
     }
   };
 }
